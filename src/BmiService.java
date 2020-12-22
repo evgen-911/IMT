df40366d8066225eb65e73f2dac1b7b51cf522d2
evgeny knyazkin <evgen.kudesnik@gmail.com>
2020-12-22 22:21:26 +0300

    reformat#2

--- src/BmiService.java
+++ src/BmiService.java
@@ -1,9 +1,8 @@
 public class BmiService {
-
     public float calculate(float bodyMass, float height) {
+
         float heightFinish = (height / 100) * (height / 100);
         float imt = bodyMass / heightFinish;
         return imt;
     }
-
 }
--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,6 @@
 public class Main {
     public static void main(String[] args) {
+
         BmiService service = new BmiService();
         float bodyMass = 80;
         float height = 180;
